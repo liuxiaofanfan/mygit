4e03056301889ee49a94bf2711c414a710902f17
liufanfan <594512881@qq.com>
2018-02-28 19:08:30 +0800

    删除一个项目

--- yii2_advanced/api/controllers/CommentController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace api\controllers;
-
-class CommentController extends \yii\rest\ActiveController
-{
-    public $modelClass = 'common\models\Comment';
-}
--- yii2_advanced/api/controllers/UserController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace api\controllers;
-
-class UserController extends \yii\rest\ActiveController
-{
-    public $modelClass = 'common\models\User';
-}
