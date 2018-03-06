844edf0f3b5ecd3aba9e42deb33f0538fbdb611c
liufanfan <594512881@qq.com>
2018-03-06 19:18:22 +0800

    登录页面修改

--- lf_center/backend/views/site/login.php
+++ lf_center/backend/views/site/login.php
@@ -31,26 +31,26 @@ $fieldOptions2 = [
         <div class="login-div clearfix">
             <div class="login-table">
                 <div class="login-label">登录</div>
-                    <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
+                <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
 
-                    <?= $form
-                        ->field($model, 'username', $fieldOptions1)
-                        ->label(false)
-                        ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+                <?= $form
+                    ->field($model, 'username', $fieldOptions1)
+                    ->label(false)
+                    ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
 
-                    <?= $form
-                        ->field($model, 'password', $fieldOptions2)
-                        ->label(false)
-                        ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+                <?= $form
+                    ->field($model, 'password', $fieldOptions2)
+                    ->label(false)
+                    ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
 
-                    <div>
-                        <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                    </div>
+                <div>
+                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                </div>
 
-                    <div>
-                        <?= Html::submitButton('Sign in', ['class' => 'btn btn-success btn-block btn-flat', 'name' => 'login-button', 'id' => 'do-login']) ?>
-                    </div>
-        <?php ActiveForm::end(); ?>
+                <div>
+                    <?= Html::submitButton('Sign in', ['class' => 'btn btn-success btn-block btn-flat', 'name' => 'login-button', 'id' => 'do-login']) ?>
+                </div>
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
@@ -59,29 +59,37 @@ $fieldOptions2 = [
             <div class="role-label">账号分类说明</div>
             <div class="row">
                 <div class="col-xs-12 col-md-4 role-item role-user">
-                    <div class="role-pic"></div>
-                    <div class="role-msg">
-                        <div class="name">普通用户</div>
-                        <div class="msg">可以进入网站前台，浏览其中的数据，使用其中的功能</div>
+                    <div class="role-item-border">
+                        <div class="role-pic"></div>
+                        <div class="role-msg">
+                            <div class="name">普通用户</div>
+                            <div class="msg">可以进入网站前台，浏览其中的数据，使用其中的功能</div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-4 role-item role-admin">
-                    <div class="role-pic"></div>
-                    <div class="role-msg">
-                        <div class="name">管理员</div>
-                        <div class="msg">可以进入网站前台、后台，使用其中的功能，支持文件上传</div>
+                    <div class="role-item-border">
+                        <div class="role-pic"></div>
+                            <div class="role-msg">
+                                <div class="name">管理员</div>
+                                <div class="msg">可以进入网站前台、后台，使用其中的功能，支持文件上传</div>
+                            </div>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-4 role-item role-god">
-                    <div class="role-pic"></div>
-                    <div class="role-msg">
-                        <div class="name">开发者</div>
-                        <div class="msg">能进入前台、后台，可以看到开发菜单</div>
+                    <div class="role-item-border">
+                        <div class="role-pic"></div>
+                        <div class="role-msg">
+                            <div class="name">开发者</div>
+                            <div class="msg">能进入前台、后台，可以看到开发菜单</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="footer"></div>
+    <div class="footer">
+        <strong>Copyright &copy; 2018 <a href="javascript:;">江苏点盈网络科技有限公司</a>.</strong> All rights reserved.
+    </div>
 </div>
 
