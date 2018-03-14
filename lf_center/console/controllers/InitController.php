c775770bc619469eea0ecfbb94260a177899c300
刘帆 <594512881@qq.com>
2018-03-14 09:56:40 +0800

    test

--- lf_center/console/controllers/InitController.php
+++ lf_center/console/controllers/InitController.php
@@ -2,6 +2,7 @@
 
 namespace console\controllers;
 
+use common\models\User;
 use yii\console\Controller;
 
 /**
@@ -18,5 +19,19 @@ class InitController extends Controller
 		$email    = $this->prompt("Input Email for $username : ");
 		$password = $this->prompt("Input Password for $username : ");
 
+		// $model = new User();
+		// $model->username = $username;
+		// $model->email    = $email;
+		// $model->password = $password;
+
+		// if(!$model->save()){
+		// 	foreach($model->getErrors() as $errors){
+		// 		foreach($errors as $e){
+		// 			echo "$e \n";
+		// 		}
+		// 	}
+		// 	return 1;
+		// }
+		return 0;
 	}
 }
