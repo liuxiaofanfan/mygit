54d0768f8747ffd6eacd841f0f4a8e7dc762128e
刘帆 <594512881@qq.com>
2018-03-14 16:10:51 +0800

    为了gii

--- lf_center/common/config/main.php
+++ lf_center/common/config/main.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'language' => "zh-CN",
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
@@ -9,5 +10,61 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+
+        #国际化支持
+        'i18n' => [
+            'translations' => [
+                'common*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'fileMap' => [
+                        'common' => 'common.php',
+                        'common/pms_user' => 'pms_user.php',
+                        'common/pms_role' => 'pms_role.php',
+                        'common/pms_menu' => 'pms_menu.php',
+                        'common/pms_role_menu' => 'pms_role_menu.php',
+                        'common/pms_user_role' => 'pms_user_role.php',
+                    ],
+                ],
+                'backend*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@backend/messages',
+                    'fileMap' => [
+                        'backend' => 'backend.php',
+                        'backend/pms_user' => 'pms_user.php',
+                        'backend.php/pms_role' => 'pms_role.php',
+                        'backend/pms_menu' => 'pms_menu.php',
+                        'backend/pms_role_menu' => 'pms_role_menu.php',
+                        'backend/pms_user_role' => 'pms_user_role.php',
+                    ],
+                ],
+                'frontend*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@frontend/messages',
+                    'fileMap' => [
+                        'frontend' => 'frontend.php',
+                        'frontend/pms_user' => 'pms_user.php',
+                        'frontend.php/pms_role' => 'pms_role.php',
+                        'frontend/pms_menu' => 'pms_menu.php',
+                        'frontend/pms_role_menu' => 'pms_role_menu.php',
+                        'frontend/pms_user_role' => 'pms_user_role.php',
+                    ],
+                ],
+                'api*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@api/messages',
+                    'fileMap' => [
+                        'api' => 'api.php',
+                        'api/section' => 'section.php',
+                        'api/comment' => 'comment.php',
+                    ],
+                ],
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages',
+                ],
+            ],
+        ],
+        
     ],
 ];
---
+++ lf_center/common/messages/zh-CN/pms_user.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'User ID' => '用户ID',
+	'User Name' => '用户名',
+	'Account' => '账号',
+	'Password' => '密码',
+	'Is Del' => '是否已经删除',
+	'Is Mod Pwd' => '初始是否修改密码',
+];
