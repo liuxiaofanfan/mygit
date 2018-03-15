5d3b45e23a7c653317e685c823ae748e162371b3
刘帆 <594512881@qq.com>
2018-03-15 14:21:17 +0800

    login css

--- lf_center/backend/views/site/login.php
+++ lf_center/backend/views/site/login.php
@@ -21,15 +21,30 @@ AppAsset::addCss($this, 'css/back_login.css');
         <div class="login-div clearfix">
             <div class="login-table">
                 <div class="login-label">登录</div>
-                <form>
-                    <div class="username-div">
-                        <i class="con-user"></i>
-                        <div></div>
+                    <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
+
+                    <?= $form
+                        ->field($model, 'username', $fieldOptions1)
+                        ->label(false)
+                        ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
+
+                    <?= $form
+                        ->field($model, 'password', $fieldOptions2)
+                        ->label(false)
+                        ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+
+                    <div class="row">
+                        <div class="col-xs-8">
+                            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-xs-4">
+                            <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
+                        </div>
+                        <!-- /.col -->
                     </div>
-                    <div class="password-div"></div>
-                    <div class="rem-div"></div>
-                    <div class="do-login"></div>
-                </form>
+
+        <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
