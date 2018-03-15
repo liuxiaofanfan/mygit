4caffeb86a538756e721eb0f6d2fe8ccf7d509cf
刘帆 <594512881@qq.com>
2018-03-15 11:43:38 +0800

    css

--- lf_center/backend/assets/AppAsset.php
+++ lf_center/backend/assets/AppAsset.php
@@ -21,4 +21,12 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
+    public static function addScript($view, $jsfile) {  
+        $view->registerJsFile($jsfile, [AppAsset::className(), "depends" => "backend\assets\AppAsset"]);  
+    }  
+    //定义按需加载css方法，注意加载顺序在最后  
+    public static function addCss($view, $cssfile) {  
+        $view->registerCssFile($cssfile, [AppAsset::className(), "depends" => "backend\assets\AppAsset"]);  
+    }  
 }
--- lf_center/backend/views/site/login.php
+++ lf_center/backend/views/site/login.php
@@ -1,6 +1,7 @@
 <?php
-use yii\helpers\Html;
+use backend\assets\AppAsset;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
@@ -8,7 +9,8 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Sign In';
 
-$this->registerCssFile("@backend/web/css/back_login.css");
+AppAsset::register($this);
+AppAsset::addCss($this, Yii::$app->request->baseUrl.'/css/back_login.css');
 ?>
 
 <div class="app">
