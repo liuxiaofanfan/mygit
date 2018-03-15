a13f0b880aaf005a65ccb0952c0cf5c6bb56a9f0
刘帆 <594512881@qq.com>
2018-03-15 11:28:58 +0800

    css

--- lf_center/backend/views/site/login.php
+++ lf_center/backend/views/site/login.php
@@ -8,7 +8,7 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Sign In';
 
-$this->registerCssFile("@web/css/back_login.css");
+$this->registerCssFile("@backend/web/css/back_login.css");
 ?>
 
 <div class="app">
