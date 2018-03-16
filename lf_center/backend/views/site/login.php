e7d57ff5e8138050b5d58b685b2036eb8ba319e6
刘帆 <594512881@qq.com>
2018-03-16 11:25:27 +0800

    login css update

--- lf_center/backend/views/site/login.php
+++ lf_center/backend/views/site/login.php
@@ -25,7 +25,7 @@ $fieldOptions2 = [
 
 <div class="app">
     <div class="header">
-        <div class="header-content clearfix">
+        <div class="header-content">
             <div class="head-left">
                 <span class="icon"></span>
                 <span class="name">点盈后台管理平台</span>
