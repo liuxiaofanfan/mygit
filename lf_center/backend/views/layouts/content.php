4e00ae1a087d43c503db21c64a9c9fa31fd9760a
刘帆 <594512881@qq.com>
2018-03-17 14:31:24 +0800

    多主题支持

--- lf_center/backend/views/layouts/content.php
+++ lf_center/backend/views/layouts/content.php
@@ -1,6 +1,7 @@
 <?php
-use yii\widgets\Breadcrumbs;
 use dmstr\widgets\Alert;
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
 
 ?>
 <div class="content-wrapper">
@@ -11,10 +12,10 @@ use dmstr\widgets\Alert;
             <h1>
                 <?php
                 if ($this->title !== null) {
-                    echo \yii\helpers\Html::encode($this->title);
+                    echo Html::encode($this->title);
                 } else {
-                    echo \yii\helpers\Inflector::camel2words(
-                        \yii\helpers\Inflector::id2camel($this->context->module->id)
+                    echo Inflector::camel2words(
+                        Inflector::id2camel($this->context->module->id)
                     );
                     echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
                 } ?>
