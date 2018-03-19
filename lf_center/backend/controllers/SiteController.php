1d77435096665baab5460a20e652ca4f4163f2d6
刘帆 <594512881@qq.com>
2018-03-19 19:18:49 +0800

    rbac

--- lf_center/backend/controllers/SiteController.php
+++ lf_center/backend/controllers/SiteController.php
@@ -23,7 +23,7 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['login', 'error', 'language', 'set-theame'],
+                        'actions' => ['login', 'error', 'language', 'set-theame', 'test'],
                         'allow' => true,
                     ],
                     [
