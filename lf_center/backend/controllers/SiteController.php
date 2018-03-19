cdcebe88ea8e31e2901ee2a080ced52a408c7aef
刘帆 <594512881@qq.com>
2018-03-19 20:35:54 +0800

    rbac

--- lf_center/backend/controllers/SiteController.php
+++ lf_center/backend/controllers/SiteController.php
@@ -71,7 +71,7 @@ class SiteController extends Controller
      * @return [type] [description]
      */
     public function actionAssignMenu(){
-        echo json_encode(MenuHelper::getAssignedMenu(Yii::$app->user->identity));
+        echo json_encode(MenuHelper::getAssignedMenu(Yii::$app->user->identity));exit;
     }
 
     /**
