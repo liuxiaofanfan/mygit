9e6a59b39e8515461d9d9d722b1340922845a0b4
刘帆 <594512881@qq.com>
2018-03-20 17:59:51 +0800

    pjax优化

--- lf_center/backend/views/lf-pms-menu/index.php
+++ lf_center/backend/views/lf-pms-menu/index.php
@@ -33,7 +33,13 @@ $this->params['breadcrumbs'][] = $this->title;
             'target_url:url',
             'sequence',
             'icon',
-            ['class' => 'yii\grid\ActionColumn'],
+            ['class' => 'yii\grid\ActionColumn',
+            'view' => function ($url, $model) {
+                return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'data-pjax'=>'',
+                ]);
+            }],
         ],
     ]); ?>
     <?php Pjax::end(); ?>
