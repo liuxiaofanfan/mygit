cb42f0711d0520ea2688361f8833fd7fac7481d3
刘帆 <594512881@qq.com>
2018-03-20 17:21:10 +0800

    pjax优化

--- lf_center/backend/views/lf-pms-menu/index.php
+++ lf_center/backend/views/lf-pms-menu/index.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="lf-pms-menu-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php Pjax::begin(); ?>
+    <?php Pjax::begin(['id' => 'main-content']); ?>
     <?php //echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
