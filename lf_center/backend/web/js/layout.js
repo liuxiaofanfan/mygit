8c4cf55c3b61e1a75d191177f04d93b5b6a535af
刘帆 <594512881@qq.com>
2018-03-21 17:59:24 +0800

    pjax

--- lf_center/backend/web/js/layout.js
+++ lf_center/backend/web/js/layout.js
@@ -1,5 +1,5 @@
 $(function(){
-	$(document).pjax('ul.sidebar-menu.tree a, .content a', '#out-content', {"push":true,"replace":false,"timeout":1000,"scrollTo":false,"container":"#out-content"});
+	$(document).pjax('a, .content a', '#out-content', {"push":true,"replace":false,"timeout":1000,"scrollTo":false,"container":"#out-content"});
 
 	$(".child-menu").click(function(){
 		$(".child-menu").removeClass("active-menu");
