7780de000321aa408e72c2958fafd90fce1191e2
刘帆 <594512881@qq.com>
2018-03-21 09:58:56 +0800

    登录页也引入appasset

--- lf_center/backend/views/layouts/main-login.php
+++ lf_center/backend/views/layouts/main-login.php
@@ -4,7 +4,11 @@ use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
-
+if (class_exists('backend\assets\AppAsset')){
+    backend\assets\AppAsset::register($this);
+} else {
+    app\assets\AppAsset::register($this);
+}
 dmstr\web\AdminLteAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
