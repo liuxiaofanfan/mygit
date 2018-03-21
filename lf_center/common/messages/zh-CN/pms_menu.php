b3bee791ac44422f8b0ac6c83e93c552f6974076
刘帆 <594512881@qq.com>
2018-03-21 14:14:05 +0800

    国际化支持

---
+++ lf_center/common/messages/zh-CN/pms_menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	#模型字段
+	'Menu ID' => '菜单ID',
+    'Menu Name' => '菜单名称',
+    'Menu Level' => '菜单等级',
+    'Parent ID' => '父级菜单',
+    'Target Url' => '路由地址',
+    'Sequence' => '序列',
+    'Icon' => '图标',
+];
