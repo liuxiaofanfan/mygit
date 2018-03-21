91b93bfd46dbda6ca2a58c6cc225e81a45793efb
刘帆 <594512881@qq.com>
2018-03-21 09:59:21 +0800

    登录页找不到pjax

--- lf_center/backend/views/layouts/main-login.php
+++ lf_center/backend/views/layouts/main-login.php
@@ -4,9 +4,9 @@ use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
-if (class_exists('backend\assets\AppAsset')){
+if(class_exists('backend\assets\AppAsset')){
     backend\assets\AppAsset::register($this);
-} else {
+}else{
     app\assets\AppAsset::register($this);
 }
 dmstr\web\AdminLteAsset::register($this);
