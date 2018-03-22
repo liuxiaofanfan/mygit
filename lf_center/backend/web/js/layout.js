af10f0d2ae636b6019c11a6884de08406c8b29e0
刘帆 <594512881@qq.com>
2018-03-22 15:15:00 +0800

    1

--- lf_center/backend/web/js/layout.js
+++ lf_center/backend/web/js/layout.js
@@ -2,7 +2,11 @@ $(function(){
 	$(document).pjax('ul.sidebar-menu.tree a', '#out-content', {"push":true,"replace":false,"timeout":1000,"scrollTo":false,"container":"#out-content"});
 
 	$(document).on("pjax:end",function(){
-       
+       $("#out-content").on("click", ".delete-menu-batch", function(){
+			alert("456");
+	        var keys = $("#menu-grid").yiiGridView("getSelectedRows");
+	        console.log(keys);
+	    });
     });
 
 	$(".child-menu").click(function(){
