0983fced032f90d42b4142923f0b3b13e4844a94
刘帆 <594512881@qq.com>
2018-03-22 13:38:32 +0800

    1

--- lf_center/backend/web/js/layout.js
+++ lf_center/backend/web/js/layout.js
@@ -1,18 +1,16 @@
 $(function(){
-	// $(document).pjax('ul.sidebar-menu.tree a, .content a', '#out-content', {"push":true,"replace":false,"timeout":1000,"scrollTo":false,"container":"#out-content"});
+	$(document).pjax('ul.sidebar-menu.tree a', '#out-content', {"push":true,"replace":false,"timeout":1000,"scrollTo":false,"container":"#out-content"});
 
-	// $(document).on("pjax:end",function(){
- //        $("#out-content").on("click", ".delete-menu-batch", function(){
-	//         var keys = $("#menu-grid").yiiGridView("getSelectedRows");
-	//         console.log(keys);
-	//     });
- //    });
+	$(document).on("pjax:end",function(){
+        $("#out-content").on("click", ".delete-menu-batch", function(){
+	        var keys = $("#menu-grid").yiiGridView("getSelectedRows");
+	        console.log(keys);
+	    });
+    });
+
 	$(".child-menu").click(function(){
 		$(".child-menu").removeClass("active-menu");
 		$(this).addClass("active-menu");
-		var url = $(this).attr("href");
-        $.ajaxSetup({cache: false });
-        $(".content").load(url);
 	});
 
 });
