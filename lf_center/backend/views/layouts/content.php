f329741f0f54b710da027ffbd2f913ab268de53b
刘帆 <594512881@qq.com>
2018-03-22 15:02:24 +0800

    1

--- lf_center/backend/views/layouts/content.php
+++ lf_center/backend/views/layouts/content.php
@@ -8,12 +8,7 @@ use yii\widgets\Pjax;
 ?>
 <div class="content-wrapper">
     <?php Pjax::begin(['id' => 'out-content']); ?>
-    <?php
-        if(file_exists('js/'.Yii::$app->controller->id.'/'.Yii::$app->controller->action->id.'.js')){
-            AppAsset::register($this);
-            AppAsset::addScript($this, 'js/'.Yii::$app->controller->id.'/'.Yii::$app->controller->action->id.'.js');
-        }
-    ?>
+    
     <section class="content-header">
         <?php if (isset($this->blocks['content-header'])) { ?>
             <h1><?= $this->blocks['content-header'] ?></h1>
@@ -47,6 +42,13 @@ use yii\widgets\Pjax;
             <?php Pjax::end(); ?>
         </section>
     </section>
+
+    <?php
+        if(file_exists('js/'.Yii::$app->controller->id.'/'.Yii::$app->controller->action->id.'.js')){
+            AppAsset::register($this);
+            AppAsset::addScript($this, 'js/'.Yii::$app->controller->id.'/'.Yii::$app->controller->action->id.'.js');
+        }
+    ?>
     <?php Pjax::end(); ?>
 </div>
 
