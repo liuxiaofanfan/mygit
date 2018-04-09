7df700c1bc583c58b81f9864fc458de321ee42bf
刘帆 <594512881@qq.com>
2018-04-09 10:09:50 +0800

    图片上传

--- lf_center/backend/config/params.php
+++ lf_center/backend/config/params.php
@@ -6,4 +6,28 @@ return [
     	'0' => '-请选择图片位置-',
     	'1' => '顶部轮播',
     ],
+
+    // 图片服务器的域名设置，拼接保存在数据库中的相对地址，可通过web进行展示
+    'domain' => Yii::$app->request->hostInfo."/",
+    'imageUploadRelativePath' => './uploads/header/', // 图片默认上传的目录
+    'imageUploadSuccessPath' => 'uploads/header/', // 图片上传成功后，路径前缀
+    'webuploader' => [
+        // 后端处理图片的地址，value 是相对的地址
+        'uploadUrl' => 'site/upload',
+        // 多文件分隔符
+        'delimiter' => ',',
+        // 基本配置
+        'baseConfig' => [
+            'defaultImage' => '/pic/personal/nopic.png',
+            'disableGlobalDnd' => true,
+            'accept' => [
+                'title' => 'Images',
+                'extensions' => 'gif,jpg,jpeg,bmp,png',
+                'mimeTypes' => 'image/*',
+            ],
+            'pick' => [
+                'multiple' => false,
+            ],
+        ],
+    ],
 ];
--- lf_center/backend/views/wx-pic/_form.php
+++ lf_center/backend/views/wx-pic/_form.php
@@ -12,7 +12,8 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'PIC_URL')->textInput(['maxlength' => true]) ?>
+    <!-- <?= $form->field($model, 'PIC_URL')->textInput(['maxlength' => true]) ?> -->
+    <?= $form->field($model, 'PIC_URL')->widget('manks\FileInput', []) ?>
 
     <?= $form->field($model, 'PIC_POS')->dropDownList(Yii::$app->params['pic_position']) ?>
 
